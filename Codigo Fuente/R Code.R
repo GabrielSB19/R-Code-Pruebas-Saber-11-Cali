b343c59ec6944cfcda7656638e09058d26d11060
Gabriel Suarez <gabriel.suarez1@correo.icesi.edu.co>
2021-11-18 06:53:26 -0500

    Add test T for the average

--- Codigo Fuente/R Code.R
+++ Codigo Fuente/R Code.R
@@ -23,12 +23,19 @@ BaseData <- read_excel("C:/Users/usuario/OneDrive - Universidad Icesi (@icesi.ed
 BaseDataBecados <- filter(BaseData, ESTU_GENERACIONE == "GENERACION E - EXCELENCIA NACIONAL")
 BaseDataNoBecados <- filter(BaseData, ESTU_GENERACIONE == "NO")
 
+# Gender exploration
 PieGender <- factor(BaseDataBecados$ESTU_GENERO, labels = c("Femenino", "Masculino"))
 Genero <- table(PieGender)
 pie(Genero, main = "Becados por genero", col = c("purple", "orange"), clockwise = TRUE)
 legend("topright", c("Feminio", "Masculino"), cex = 1.5, fill = c("purple", "orange"))
 Genero.TFrecuenciaGenero <- freq(ESTU_GENERO, plot = FALSE)
-Genero.TFrecuenciaGenero
+Genero.TFrecuenciaGenero.
+
+#Hypotheses for the average
+LCData <- BaseDataNoBecados[c(10)]
+t.test(LCData, mu = 62, alternative = "less")
+
+
 
 
 
