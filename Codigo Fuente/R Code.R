6d232cf6256063df85e9bce96705012f22dea642
Johan794 <johan.ricardo1104@gmail.com>
2021-11-18 15:47:28 -0500

    Adding anova test

--- Codigo Fuente/R Code.R
+++ Codigo Fuente/R Code.R
@@ -18,7 +18,7 @@ library(descr)
 library(openxlsx)
 library(dplyr)
 
-BaseData <- read_excel("C:/Users/usuario/OneDrive - Universidad Icesi (@icesi.edu.co)/Escritorio/Universidad/Semestre 4/Inferencia Estadistica/Proyecto final Inferencia/Base de Datos/Saber_11__2019-2 Trabajo Final.xlsx")
+BaseData <- read_excel("Escritorio/ICESI/Cuarto Semestre de Ingenieria de sistemas/Talleres/Inferencia/R-workSpace/ProyectoFinalInfe/R-Code-Pruebas-Saber-11-Cali/Base de Datos/Saber_11__2019-2 Trabajo Final.xlsx")
 
 BaseDataBecados <- filter(BaseData, ESTU_GENERACIONE == "GENERACION E - EXCELENCIA NACIONAL")
 BaseDataNoBecados <- filter(BaseData, ESTU_GENERACIONE == "NO")
@@ -52,7 +52,10 @@ PData <- BaseData[c(29)]
 PDataSucces <- filter(PData, ESTU_GENERACIONE2 == 1)
 prop.test(x = nrow(PDataSucces), n = nrow(PData), p = 0.15, alternative = "less", conf.level = 0.95, correct = FALSE)
 
-
-
-
+#ANOVA Which stratum belong to the students who obtained a better score on the English test?
+attach(BaseData)
+boxplot(PUNT_INGLES ~ FAMI_ESTRATOVIVIENDA) #Delete atypical data
+anova<- aov(lm(PUNT_INGLES ~FAMI_ESTRATOVIVIENDA))
+summary(anova)
+TukeyHSD(anova )
 
