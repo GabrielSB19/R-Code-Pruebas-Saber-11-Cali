9644d961cd1adb364c423ba4d51503874cb0fb36
Gabriel Suarez <gabriel.suarez1@correo.icesi.edu.co>
2021-11-18 08:49:12 -0500

    Add test for Proportion

--- Codigo Fuente/R Code.R
+++ Codigo Fuente/R Code.R
@@ -29,13 +29,15 @@ Genero <- table(PieGender)
 pie(Genero, main = "Becados por genero", col = c("purple", "orange"), clockwise = TRUE)
 legend("topright", c("Feminio", "Masculino"), cex = 1.5, fill = c("purple", "orange"))
 Genero.TFrecuenciaGenero <- freq(ESTU_GENERO, plot = FALSE)
-Genero.TFrecuenciaGenero.
 
 #Hypotheses for the average
 LCData <- BaseDataNoBecados[c(10)]
 t.test(LCData, mu = 62, alternative = "less")
 
-
+#Hypotheses for the proportion
+PData <- BaseData[c(29)]
+PDataSucces <- filter(PData, ESTU_GENERACIONE2 == 1)
+prop.test(x = nrow(PDataSucces), n = nrow(PData), p = 0.15, alternative = "less", conf.level = 0.95, correct = FALSE)
 
 
 
